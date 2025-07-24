a2008803319fb80b68da2b70c1347c4babb60a2e
JonathanSeah <24027825@myrp.edu.sg>
2025-07-24 20:57:14 +0800

    branch?

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // Test :D
+const express = require('express'); 
+
+const app = express(); 
+
+ 
+
+app.get('/', (req, res) => { 
+
+   res.send("Hello World!") 
+
+}) 
+
+ 
+
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
