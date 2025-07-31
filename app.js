3ffaa865dd13e8b3d6eea8c34b74f4628c458e67
H-avier <simjavier323@gmail.com>
2025-07-31 16:01:26 +0800

    Testing db

--- app.js
+++ app.js
@@ -37,9 +37,7 @@ const connection = mysql.createConnection({
     port: 3306, 
     password: 'c237student!',
     database: 'c237_24009380',
-    ssl: {
-        rejectUnauthorized: true // Disable SSL certificate validation
-    }
+    
   });
 const pool = connection.promise(); 
 // Connect to the MySQL database
